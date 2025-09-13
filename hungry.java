db91e05f58f572f023ae89e954f80e36f6c56bc0
Varshitha poloju <varshithapolojy66@gmail.com>
2025-09-13 20:00:50 +0530

    added choices of hungry

--- hungry.java
+++ hungry.java
@@ -2,7 +2,19 @@ public class hungry {
 
     public static void main(String [] args)
     {
-        System.out.println("i am hungry");
+        System.out.println("are you hungry");
+        boolean hungry=true;
+
+        if(hungry)
+        {
+            System.out.println("eat shawarma");
+        }
+        else
+        {
+            System.out.println("study java");
+        }
+
+
     }
     
 }
