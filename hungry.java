7fee94539e8c0d4ac86e4b3a514d644042e72db6
Varshitha poloju <varshithapolojy66@gmail.com>
2025-09-15 11:28:37 +0530

    rollback

--- hungry.java
+++ hungry.java
@@ -7,13 +7,8 @@ public class hungry {
         boolean angry=true;
         System.out.println("heyy i added a new line here");
         System.out.println("hey may be we cam to an end");
-<<<<<<< HEAD
-        System.out.println("lets see what happens?");
-        
-=======
         System.out.println("my life my rules");
 
->>>>>>> 8ed5119148eabcb9f41d9d67f7e293b07bd3b04b
         if(hungry)
         {
             if(angry)
@@ -21,15 +16,11 @@ public class hungry {
                 System.out.println("sorry gor being late");
             }
             System.out.println("eat shawarma");
-
             System.out.println("eat panipuri");
         }
         else
         {
             System.out.println("study java");
         }
-
-
     }
-    
 }
