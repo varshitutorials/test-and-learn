2ba9f67d3953d29dbac65f6bd6fe9b5c1820776b
Varshitha poloju <165406654+varshitutorials@users.noreply.github.com>
2025-09-15 11:43:19 +0530

    Update hungry.java
    lets see

--- hungry.java
+++ hungry.java
@@ -6,9 +6,7 @@ public class hungry {
         boolean hungry=true;
         boolean angry=true;
         System.out.println("heyy i added a new line here");
-        System.out.println("hey may be we cam to an end");
-        System.out.println("my life my rules");
-        System.out.println("model bolthe");
+
 
         if(hungry)
         {
